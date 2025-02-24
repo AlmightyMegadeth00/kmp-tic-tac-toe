40f3c304f249777cb82ca25b47d0614a73a1f4c8
Dave Kessler <dkessler@lesuto.com>
2025-02-23 20:26:19 -0600

    Medium difficulty added to skynet.  It looks for winning moves

--- composeApp/src/commonMain/kotlin/org/kmptictactoe/project/utils/SkynetCpu.kt
+++ composeApp/src/commonMain/kotlin/org/kmptictactoe/project/utils/SkynetCpu.kt
@@ -1,6 +1,7 @@
 package org.kmptictactoe.project.utils
 
 import org.kmptictactoe.project.Difficulty
+import org.kmptictactoe.project.utils.ValidatorUtils.checkForWinner
 import kotlin.random.Random
 
 interface SkynetCpu {
@@ -16,14 +17,18 @@ class SkynetCpuImpl: SkynetCpu {
         private val TAG = SkynetCpuImpl::class.simpleName
     }
 
-    private var difficulty: Difficulty = Difficulty.EASY
+    private var difficulty: Difficulty = Difficulty.MEDIUM
 
+    /**
+     * EASY: Computer will move using just random numbers remaining from moves list
+     * MEDIUM: Computer will pick moves using random numbers, but complete the game when 2 in a row are found
+     * HARD: Computer will favor moves that offer inherent advantages, play defensively if the player has
+     * 2 cells in a row, and complete the game if it has 2 cells in a row
+     */
     override fun setDifficulty(difficulty: Difficulty) {
         this.difficulty = difficulty
     }
 
-    // TODO: split this logic into internal methods for difficulty and supply parameters
-    //  between the viewmodel and ROOM storage
     override fun getNextMove(
         moveIndexesRemaining: Set<Int>,
         currentPlayerMovesSet: Set<Int>,
@@ -34,13 +39,36 @@ class SkynetCpuImpl: SkynetCpu {
                 getRandomNextMoveIndex(moveIndexesRemaining)
             }
             Difficulty.MEDIUM -> {
-                0
+                getWinningMoveIndexOrNext(moveIndexesRemaining, currentPlayerMovesSet)
             }
             Difficulty.HARD -> {
                 0
             }
 
         }
+    }
+
+    /**
+     * Consider moves that win the game
+     */
+    private fun getWinningMoveIndexOrNext(moveIndexesRemaining: Set<Int>, playerMovesArray: Set<Int>): Int {
+        var nextMoveIndex = getRandomNextMoveIndex(moveIndexesRemaining)
+        for (value in moveIndexesRemaining) {
+            if (canWinWithNextTurn(value, playerMovesArray)) {
+                nextMoveIndex = value
+                break
+            }
+        }
+        return nextMoveIndex
+    }
+
+    /**
+     * Determine if next move will win
+     */
+    private fun canWinWithNextTurn(nextMove: Int, playerMovesSet: Set<Int>): Boolean {
+        val exTreatment = playerMovesSet.toMutableSet()
+        exTreatment.add(nextMove)
+        return checkForWinner(exTreatment)
     }
 
     /**
